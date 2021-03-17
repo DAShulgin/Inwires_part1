58865e771f4f06ad6b8987c895f9b54c4ed9e979
Denis Shulgin <d.a.shulgin2019@ya.ru>
2021-03-17 16:09:11 +0300

    create React_App/grid

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => {
+
+  return (
+    <div className='grid'>
+
+      <div className='blok1'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque blanditiis quaerat debitis distinctio fugiat dolorum vel aut et nisi necessitatibus.
+      </div>
+
+      <div className='blok2'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repellendus sunt velit fugiat magnam inventore excepturi suscipit architecto repudiandae enim?
+       </div>
+
+      <div className='blok3'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt aspernatur dolores harum facilis veniam sint sit, molestias fugiat voluptate nisi assumenda tenetur cumque ipsa architecto, porro nesciunt laborum distinctio libero.
+        Molestiae aperiam tempore, provident laudantium ab tempora aspernatur sit recusandae! Necessitatibus deserunt harum voluptate, odio laudantium reiciendis facilis repudiandae id quisquam quia, officiis minima similique officia, dolor consectetur modi nam.
+        Quidem cum distinctio odit vel nostrum, ex, error possimus sunt aperiam inventore pariatur quae deserunt quaerat fugit atque aspernatur impedit laudantium modi. Praesentium rerum ratione quo quibusdam mollitia tenetur neque.
+        Odit omnis, odio exercitationem labore inventore sint voluptas! Quas officiis quibusdam aut, similique eligendi reiciendis accusamus illum nisi aliquid temporibus facilis et repellat corrupti, unde quos veniam neque doloribus! Aliquam?
+        Ipsa ab at cupiditate sed nesciunt quaerat, dolor nulla, reiciendis itaque, porro vitae? Harum odit dolorem facilis fugiat, corrupti illo illum dolor nesciunt quasi, tenetur nulla quos ullam officiis ut.
+        Molestias, quae? Recusandae architecto dicta odit doloribus! Dolores impedit pariatur voluptatem ad, sunt reprehenderit fugit, quisquam provident quaerat odit sapiente? Totam officiis repudiandae error, cumque consectetur ea culpa cum sequi?
+        Laudantium blanditiis dicta autem aspernatur eligendi labore commodi repellat, voluptatum dolor vitae? Totam expedita voluptatibus cumque maxime, enim quidem perferendis tempora voluptates, distinctio minus ad mollitia officiis culpa accusamus maiores.
+        Aperiam, odit. Veniam voluptatibus, expedita, cumque labore esse eligendi in nihil atque facere dolores molestiae sunt ullam cum nam provident repudiandae iure ea incidunt pariatur quam possimus rem? Magnam, quae.
+        Culpa nisi quidem exercitationem, nesciunt vero ipsam nulla itaque! A autem dolores eum, earum provident voluptas, saepe, ullam molestias sequi labore nihil dolore dignissimos tempore modi iusto nobis nulla? Omnis.
+        Sapiente nobis reprehenderit maiores tempora quo quaerat, natus eveniet iste vel, molestiae hic! Commodi vitae tenetur praesentium, architecto eveniet voluptates officiis quidem iste nisi eum reprehenderit facilis amet, expedita voluptatibus.
+  </div>
+
+    </div>
+  );
+}
+
+export default App;
